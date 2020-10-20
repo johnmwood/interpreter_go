7e4874148292cd4af8a034d02dc08b6136a935d2
John Wood <John@Johns-MacBook.attlocal.net>
2020-10-20 08:41:36 -0500

    Added repl and main.go

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"interpreter_go/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n",
+		user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+
+	repl.Start(os.Stdin, os.Stdout)
+}
