1629fab98c55c82c4daa10b99ced2edc90797458
John Wood <jmw.home@gmail.com>
2020-11-03 21:00:30 -0600

    intial Parser infix and prefix data structures

--- parser/parser.go
+++ parser/parser.go
@@ -7,12 +7,20 @@ import (
 	"interpreter_go/token"
 )
 
-type Parser struct {
-	l *lexer.Lexer
+type (
+	prefixParseFn func() ast.Expression
+	infixParseFn  func(ast.Expression) ast.Expression
+)
 
-	errors    []string
+type Parser struct {
+	l      *lexer.Lexer
+	errors []string
+
 	curToken  token.Token
 	peekToken token.Token
+
+	prefixParseFns map[token.TokenType]prefixParseFn
+	infixParseFns  map[token.TokenType]infixParseFn
 }
 
 func New(l *lexer.Lexer) *Parser {
@@ -120,4 +128,12 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.peekError(t)
 		return false
 	}
+}
+
+func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
+	p.prefixParseFns[tokenType] = fn
+}
+
+func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
+	p.infixParseFns[tokenType] = fn
 }
