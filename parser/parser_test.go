f85d6e9cc77135e6b45c77c91536fdaaa02bf57b
John Wood <jmw.home@gmail.com>
2020-11-05 09:01:34 -0600

    Syntax fixes

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -119,15 +119,15 @@ func TestIdentifierExpression(t *testing.T) {
 
 	l := lexer.New(input)
 	p := New(l)
-	program := p.ParseProgram() 
-	checkParserErrors(t, p) 
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
 
-	if len(program.Statements) := 1 {
-		t.Fatalf("program has not enough statements. got=%d", 
+	if len(program.Statements) != 1 {
+		t.Fatalf("program has not enough statements. got=%d",
 			len(program.Statements))
 	}
 
-	stmt, ok := program.Statements[0].(&ast.ExpressionStatement)
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
 	if !ok {
 		t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T",
 			program.Statements[0])
